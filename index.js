e700f7b0473db4f47581157259b5d8f395601d67
alfredo <alfredofredi1709@gmail.com>
2022-09-16 16:59:42 -0500

    Longest Word

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-function QuestionsMarks(str) {
-  let arr = str.split('');
-  let res = false;
-  let num = [];
-  let contador = 0;
-  
-  arr.map(e => {
-    if (!isNaN(e) && contador < 3) {
-      num.push(e); contador = 0;
-      if (num.length == 2 && (Number(num[0])+ Number(num[1])) == 10) {
-        res = false;
-      }
-    }
-    if (e == "?") { contador++; }
-    if ((!isNaN(e)) && contador == 3 && (Number(e) + Number(num[0])) == 10) {
-      res = true;
-      num.push(e);
-      num.shift();
-      contador = 0;
+function LongestWord(sen) {
+  var rex = /[a-z0-9]+/gi;
+  let n = sen.match(rex);
+  let anterior = 0;
+  let indice = 0;
+
+  n.map((item, index) => {
+    if (item.length >anterior) {
+      anterior = item.length;
+      indice = index;
     }
   })
 
-  return res;
+  return n[indice];
+
 }
 
 
 // keep this function call here 
-console.log(QuestionsMarks("9???1???9??1???9"));
+console.log(QuestionsMarks("hello world"));
 
 //***************Links de investigacion****************
-//https://www.delftstack.com/es/howto/javascript/check-if-string-is-number-javascript/
-//https://www.delftstack.com/es/howto/javascript/convert-string-to-number-javascript/#utilice-la-funci%25C3%25B3n-number-para-convertir-una-cadena-en-un-n%25C3%25BAmero-en-javascript
-//https://www.javascripttutorial.net/javascript-array-shift/
+// http://w3.unpocodetodo.info/utiles/regex-en-javascript.php
