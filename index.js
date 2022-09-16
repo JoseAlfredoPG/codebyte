e8045bfb4c38c6f964513bc12b1cf3fa03a7ec6a
alfredo <alfredofredi1709@gmail.com>
2022-09-15 19:36:45 -0500

    Questions Marks

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-function CodelandUsernameValidation(str) {
-  let resultado = false;
-  var expre = new RegExp("^[A-Z a-z][0-9A-Za-z/_]{2,23}[0-9A-Za-z]{1}$")
-
-  if ( expre.test(str)) {
-    resultado=true;
-  }
-  return resultado;
-}
+function QuestionsMarks(str) {
+  let arr = str.split('');
+  let res = false;
+  let num = [];
+  let contador = 0;
   
-  // keep this function call here 
-  console.log(CodelandUsernameValidation("uh"));
+  arr.map(e => {
+    if (!isNaN(e) && contador < 3) {
+      num.push(e); contador = 0;
+      if (num.length == 2 && (Number(num[0])+ Number(num[1])) == 10) {
+        res = false;
+      }
+    }
+    if (e == "?") { contador++; }
+    if ((!isNaN(e)) && contador == 3 && (Number(e) + Number(num[0])) == 10) {
+      res = true;
+      num.push(e);
+      num.shift();
+      contador = 0;
+    }
+  })
 
-  // ********* Links de investigacion *************
-  // http://w3.unpocodetodo.info/utiles/regex-en-javascript.php
-  // https://code.tutsplus.com/es/tutorials/8-regular-expressions-you-should-know--net-6149
-  // https://www.adictosaltrabajo.com/2015/01/29/regexsam/
+  return res;
+}
+
+
+// keep this function call here 
+console.log(QuestionsMarks("9???1???9??1???9"));
+
+//***************Links de investigacion****************
+//https://www.delftstack.com/es/howto/javascript/check-if-string-is-number-javascript/
+//https://www.delftstack.com/es/howto/javascript/convert-string-to-number-javascript/#utilice-la-funci%25C3%25B3n-number-para-convertir-una-cadena-en-un-n%25C3%25BAmero-en-javascript
+//https://www.javascripttutorial.net/javascript-array-shift/
